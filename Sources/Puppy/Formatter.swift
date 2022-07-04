cdbdca0c5328c6c597c82f6f9295bf861e7ae357
Marin Todorov <marin@underplot.com>
2022-07-04 18:23:25 +0200

    Safely caches used date formatters

--- Sources/Puppy/Formatter.swift
+++ Sources/Puppy/Formatter.swift
@@ -11,9 +11,17 @@ extension LogFormattable {
 }
 
 public func dateFormatter(_ date: Date, locale: String = "en_US_POSIX", dateFormat: String = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", timeZone: String = "") -> String {
+    let formatterKey = locale + dateFormat + timeZone
+    if let formatter = formatterQueue.sync(execute: { formatters[formatterKey] }) {
+        return formatter.string(from: date)
+    }
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: locale)
     dateFormatter.dateFormat = dateFormat
     dateFormatter.timeZone = TimeZone(identifier: timeZone)
+    formatterQueue.sync(flags: .barrier, execute: { formatters[formatterKey] = dateFormatter })
     return dateFormatter.string(from: date)
 }
+
+private let formatterQueue = DispatchQueue(label: "formatter", qos: .default, attributes: .concurrent)
+private var formatters = [String: DateFormatter]()
